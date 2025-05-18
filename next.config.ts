6ee42f311b9f61883a68fcc9516c1b20907a9b6a
MS_btff <whdghks845@gmail.com>
2025-05-18 19:54:28 +0900

    fix: 빌드 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  optimizeFonts: true,
+  // optimizeFonts: true,
   webpack: (config) => {
     config.externals = [...config.externals, {canvas: 'canvas'}];
     return config;
